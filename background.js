83a030bae61c4aa1d73a1a1ae60a4d163cf7ed05
Leonel Ramos <lramos@terpmail.umd.edu>
2018-09-19 05:46:50 -0400

    bug fixes: removed lone )

--- background.js
+++ background.js
@@ -2,10 +2,12 @@ var isBubly = false;
 
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
-function buttonClicked (tab) {
+function buttonClicked(tab) {
+   console.log(tab);
    isBubly = !isBubly;
    let status = {
       mode: isBubly
    }
+   console.log(status.mode);
    chrome.tabs.sendMessage(tab.id, status);
 }
--- content.js
+++ content.js
@@ -4,32 +4,31 @@ var images = document.getElementsByTagName('img');
 chrome.runtime.onMessage.addListener(gotStatus);
 
 function gotStatus(status, sender, sendResponse) {
-   console.log("status recieved");
+   console.log(`status recieved: isBubly -> ${status.mode}`);
    if(status.mode){
-      for(elt of images){
-         context = getContext(img.url);
-         data = context.getImageData(0, 0, img.width, img.hiehgt);
-         console.log(data.data);
-      }
+      console.log(images);
+      // for(let index of images){
+      //    console.log(images[index].width);
+      // }
    }else{
       /* Turn off bubbles */
    }
 }
 /* Returns a context with an image from the url */
-function getContex(url) {
-   var img = new image();
+function imgContext(url) {
+   var img = document.createElement("img");
    img.src = url;
    var canvas = document.createElement('canvas');
-   var context = canvas.getContex('2d');
+   var context = canvas.getContext('2d');
    context.drawImage(img, 0, 0);
    return context;
 }
 /* Given an array of 4 8bit integers, representing an r,g,b,a value respectivley
  * creates a **pseudo-unique key **not mathematically tested to make unique keys
  */
- function getRgbaKey(rgbaArr) {
-    var r = rgbaArr[0] * (1 << 24));
-    var g = (rgbaArr[1] && r) << 16;
-    var b = (rgbaArr[2] && r) << 8;
-    return r + g + b + a;
+function genRgbaKey(rgba) {
+    var r = rgba[0] * (1 << 24);
+    var g = rgba[1] << 16;
+    var b = rgba[2] << 8;
+    return r + g + b + rgba[3];
 }
