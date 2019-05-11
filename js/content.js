1656b2f4e35ed3a140f75b6be6c4986b5b04724a
Leonel Ramos <lramos@terpmail.umd.edu>
2019-05-11 04:18:43 -0400

    Implemented > Bub.ly algorithm with promises for speed

--- js/content.js
+++ js/content.js
@@ -1,49 +1,63 @@
-var color_distribution = new Map();
-var images = document.getElementsByTagName('img');
+let color_distribution = new Map();
+let images = [...document.getElementsByTagName('img')];
+start_bubly();
 
-chrome.runtime.onMessage.addListener(gotStatus);
-
-function gotStatus(status, sender, sendResponse) {
-   console.log(`status recieved: isBubly -> ${status.mode}`);
-   console.log(images.length);
-   if(status.mode){
-      for(let img of images){
-         console.log(img);
-         context = imgContext(img.url);
-         /* skip undefined or NaN contexts */
-         if(context){
-            console.log(context);
-         }
-
-      }
-      console.log("end of images");
-   }else{
-      /* Turn off bubbles */
-   }
-}
-function processImg(img)
+function start_bubly() 
 {
-
+   forAsync(images, (img, idx) => {
+      return new Promise(resolve => {
+         if(img.height > 100 && img.width > 100) {
+            let img_data = get_img_data(img.src);
+            process_img_data(img_data);
+         }
+         resolve();
+      })
+   });
 }
 
-function storeImgData(imgContext){
-
+function process_img_data(img_data) {
+   for (let i = 0; i < img_data.data.length; i += 4) {
+      let rgba = [img_data.data[i + 0], img_data.data[i + 1], img_data.data[i + 2], img_data.data[i + 3]];
+      let rgba_key = gen_rgba_key(rgba);
+      if(color_distribution.has(rgba_key)) color_distribution.set(rgba_key, color_distribution.get(rgba_key) + 1);
+      else color_distribution.set(rgba_key, 1);
+    }
 }
-/* Returns a context with an image from the url */
-function imgContext(url) {
-   var img = document.createElement("img");
+
+/* Returns data of input image */
+function get_img_data(url) 
+{
+   let img = document.createElement("img");
    img.src = url;
-   var canvas = document.createElement('canvas');
-   var context = canvas.getContext('2d');
+   let canvas = document.createElement('canvas');
+   let context = canvas.getContext('2d');
    context.drawImage(img, 0, 0);
-   return context;
+   return context.getImageData(0, 0, img.width, img.height);
 }
-/* Given an array of 4 8bit integers, representing an r,g,b,a value respectivley
+/* given an array of 4 8bit integers, representing an r,g,b,a value respectivley
  * creates a **pseudo-unique key **not mathematically tested to make unique keys
  */
-function genRgbaKey(rgba) {
-    var r = rgba[0] * (1 << 24);
-    var g = rgba[1] << 16;
-    var b = rgba[2] << 8;
-    return r + g + b + rgba[3];
+function gen_rgba_key(rgba) 
+{
+   let r = rgba[0] * (1 << 24);
+   let g = rgba[1] << 16;
+   let b = rgba[2] << 8;
+   return r + g + b + rgba[3];
+}
+
+/* Function Author: Stijn de Witt
+ * GitHub: https://github.com/Download/for-async
+ */
+function forAsync(arr, work) {
+	function loop(arr, i) {
+		return new Promise((resolve, reject) => {
+			if (i >= arr.length) {resolve()}
+			else try {
+				Promise.resolve(work(arr[i], i))
+				.then(() => resolve(loop(arr, i+1)))
+				.catch(reject);
+			} catch(error) {reject(error)}
+		})
+	}
+	return loop(arr, 0);
 }
