14a5799c11f24729169c3a7e90388feb45c709f7
Leonel Ramos <lramos@terpmail.umd.edu>
2019-05-11 10:16:08 -0400

    Added > communication between extension and active webpage

---
+++ js/popup.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("bubly-btn").addEventListener("click", function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {start_bubly: true, save_data: false}, function(response) {
+                alert(response.status);
+            });
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("save-btn").addEventListener("click", function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {start_bubly: false, save_data: true}, function(response) {
+                alert(response.status);
+            });
+        });
+    });
+});
