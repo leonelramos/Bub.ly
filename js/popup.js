2286bcddb6ddf7f63ffda48fd6b7b58415fbe0e7
Leonel Ramos <lramos@terpmail.umd.edu>
2019-05-11 23:17:39 -0400

    Updated > completed refactor of popup.js for clarity

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,30 @@
+let start_bubly = function ()
+{
+    let message = {start_bubly: true, save_data: false};
+    let callback = function (response) {
+        alert(response.status);
+    }
+    send_message(message, callback);
+}
+
+let save_data = function ()
+{
+    let message = {start_bubly: false, save_data: true};
+    let callback = function (response) {
+        alert(response.status);
+    }
+    send_message(message, callback);
+}
+
+let send_message = function (message, callback)
+{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, callback);
+    });
+}
+
 /* Signal content script to start Bub.ly */
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("bubly-btn").addEventListener("click", function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {start_bubly: true, save_data: false}, function(response) {
-                
-            });
-        });
-    });
-    document.getElementById("save-btn").addEventListener("click", function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {start_bubly: false, save_data: true}, function(response) {
-                
-            });
-        });
-    });
+    document.getElementById("bubly-btn").addEventListener("click", start_bubly);
+    document.getElementById("save-btn").addEventListener("click", save_data);
 });
-
